3b6cbab57be7fe12ba11582af1771070aa6cd80a
Ginja <virginija.meilune@gmail.com>
2022-08-14 14:41:26 +1000

    Working with JS

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Testing")
+const menu = document.getElementById("menu-icon");
+const exit = document.getElementById("menu-exit");
+const menuBlock = document.getElementByClass("navigation-items");
+
+function menuNavigation() {
+    menu.onclick( () => {
+        menuBlock.style.display = "flex";
+        menu.hidden = true;
+    })
+    exit.onclick( () => {
+        menuBlock.style.display = "none";
+        exit.hidden = true;
+    })
+}
+
+menuNavigation();
