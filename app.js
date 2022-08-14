1c877531cdd512e7c14ab9f4200d363ce25cf169
Ginja <virginija.meilune@gmail.com>
2022-08-14 15:01:24 +1000

    Commiting JS changes to make the navigation work

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const menu = document.getElementById("menu-icon");
 const exit = document.getElementById("menu-exit");
-const menuBlock = document.getElementByClass("navigation-items");
+const menuBlock = document.getElementById("nav-items");
 
-function menuNavigation() {
-    menu.onclick( () => {
-        menuBlock.style.display = "flex";
-        menu.hidden = true;
-    })
-    exit.onclick( () => {
+function menuNav() {
+    if(menuBlock.style.display === "flex") {
         menuBlock.style.display = "none";
-        exit.hidden = true;
-    })
+        menu.style.display = "inline-block";
+        exit.style.display = "none";
+    } else {
+        menuBlock.style.display = "flex";
+        menu.style.display = "none";
+        exit.style.display = "inline-block";
+    }
 }
-
-menuNavigation();
